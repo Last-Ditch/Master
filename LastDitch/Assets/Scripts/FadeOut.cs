dd30e1d92856358947400a63b48e84812c97c7d2
Oisin <C17343216@mydit.ie>
2019-10-09 17:41:59 +0100

    Added some UI stuff

---
+++ LastDitch/Assets/Scripts/FadeOut.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<CanvasGroup>().alpha -= 0.01f;
+        if(GetComponent<CanvasGroup>().alpha <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
