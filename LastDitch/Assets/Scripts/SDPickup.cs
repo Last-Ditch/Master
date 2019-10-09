ca9afa41a5779731b87156e086f4f0e4f97db4b8
Oisin <C17343216@mydit.ie>
2019-10-09 18:36:28 +0100

    SD Card Done

---
+++ LastDitch/Assets/Scripts/SDPickup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SDPickup : MonoBehaviour
+{
+
+    GameObject sdSlot;
+    public bool haveCard = false;
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && sdSlot!=null)
+        {
+            Debug.Log("2");
+            if (sdSlot.gameObject.GetComponent<MeshRenderer>().enabled)
+            {
+                sdSlot.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                haveCard = true;
+                return;
+            }
+            if (!sdSlot.gameObject.GetComponent<MeshRenderer>().enabled && haveCard)
+            {
+                sdSlot.gameObject.GetComponent<MeshRenderer>().enabled = true;
+                haveCard = false;
+                return;
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "SD card")
+        {
+            Debug.Log("1");
+            sdSlot = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "SD card")
+        {
+            Debug.Log("1");
+            sdSlot = null;
+        }
+    }
+}
