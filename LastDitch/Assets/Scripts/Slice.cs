b551f2056d3358bb867c359aaa4e79a7c2792c58
Oisin <C17343216@mydit.ie>
2019-10-15 18:46:55 +0100

    Slicr Updates

--- LastDitch/Assets/Scripts/Slice.cs
+++ LastDitch/Assets/Scripts/Slice.cs
@@ -1,12 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
 public class Slice : MonoBehaviour
 {
+
+    public GameObject exportPanel;
+    public Slider progressbar;
+
+    bool exporting;
+
+    private void Update()
+    {
+        if(exporting)
+        {
+            
+
+
+        }
+    }
+
+
+
     public void Sliced()
     {
+        exportPanel.SetActive(true);
+        StartCoroutine(ExportingFile());
+    }
 
-        GameObject.FindGameObjectWithTag("Progress").GetComponent<ProgressTracker>().sliced = true;
+
+    IEnumerator ExportingFile()
+    {
+        yield return new WaitForSeconds(.5f);
+        Debug.Log(progressbar.value);
+        progressbar.value +=10;
+        if (progressbar.value == 100)
+        {
+            GameObject.FindGameObjectWithTag("Progress").GetComponent<ProgressTracker>().sliced = true;
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            StartCoroutine(ExportingFile());
+        }
     }
 }
