ebddfa7159c90b41206190b7fa6b4420fa6045c1
Oisin <C17343216@mydit.ie>
2019-10-23 19:49:29 +0100

    Destruction

--- LastDitch/Assets/Scripts/Destruction.cs
+++ LastDitch/Assets/Scripts/Destruction.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Destruction : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Transform[] parts;
+
     void Start()
     {
-        
+        parts = GetComponentsInChildren<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            foreach(Transform t in parts)
+            {
+                if(t.gameObject.tag == "part")
+                {
+                    t.transform.parent = null;
+                    t.GetComponent<Rigidbody>().isKinematic = false;
+                    t.GetComponent<Rigidbody>().useGravity = true;
+                    t.GetComponent<Collider>().enabled = true;
+                }
+            }
+            StartCoroutine(Countdown());
+        }
+    }
+
+
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(5);
+        foreach (Transform t in parts)
+        {
+            Destroy(t.gameObject);
+        }
     }
 }
