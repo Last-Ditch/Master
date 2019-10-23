19c19d38904bca9910f3ec18a9078e9bc2eca737
Oisin <C17343216@mydit.ie>
2019-10-23 18:50:56 +0100

    Pickup

---
+++ LastDitch/Assets/Scripts/ObjectPickup.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPickup : MonoBehaviour
+{
+
+    public GameObject model;
+    public GameObject hand;
+    public GameObject head;
+    bool pickup;
+    Rigidbody rb;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (model != null)
+            {
+                rb = model.GetComponent<Rigidbody>();
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+                rb.useGravity = false;
+                model.GetComponent<Collider>().enabled = false;
+                model.transform.parent = null;
+                pickup = true;
+                model.transform.position = hand.transform.position;
+                model.transform.parent = head.transform;
+            }
+        }
+
+        if (model != null && pickup)
+        {
+            model.transform.position = hand.transform.position;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Throw();
+        }
+    }
+
+
+    void Throw()
+    {
+        pickup = false;
+        model.GetComponent<Collider>().enabled = true;
+        model.transform.parent = null;
+        model.GetComponent<Rigidbody>().AddForce(hand.transform.forward * 4000);
+        model.GetComponent<Rigidbody>().useGravity = true;
+
+    }
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Interactable")
+        {
+            model = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Interactable")
+        {
+            model = null;
+        }
+    }
+
+}
