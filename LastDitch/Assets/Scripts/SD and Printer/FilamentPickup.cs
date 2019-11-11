3626916a4ebdf6a714a50b07dc31dee7a6bfde18
OisinR <oisinR101@gmail.com>
2019-11-11 13:55:35 +0000

    Start of filament changes

---
+++ LastDitch/Assets/Scripts/SD and Printer/FilamentPickup.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilamentPickup : MonoBehaviour
+{
+
+    public GameObject filament;
+    public GameObject hand;
+    public GameObject head;
+    bool pickup;
+    Rigidbody rb;
+
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (filament != null)
+            {
+                filament.GetComponent<Collider>().enabled = false;
+                filament.transform.parent = null;
+                pickup = true;
+                filament.transform.position = hand.transform.position;
+                filament.transform.parent = head.transform;
+            }
+        }
+
+        if (filament != null && pickup)
+        {
+            filament.transform.position = hand.transform.position;
+        }
+
+        if (Input.GetMouseButtonDown(1) && filament != null)
+        {
+            Place();
+        }
+    }
+
+
+    void Place()
+    {
+
+    }
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Filament")
+        {
+            filament = other.gameObject;
+
+        }
+        if(other.gameObject.tag == "Ultimaker Back")
+        {
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Filament")
+        {
+            filament = null;
+        }
+
+        if (other.gameObject.tag == "Ultimaker Back")
+        {
+
+        }
+    }
+}
