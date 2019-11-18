c44ed18e1906e7802a35b339126ef15cc26e6b61
Dylberto <dylang565@gmail.com>
2019-11-18 11:32:15 +0000

    material info

---
+++ LastDitch/Assets/Scripts/UI Scripts/text_appear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class text_appear : MonoBehaviour
+{
+    public GameObject info_text;
+
+
+    public void Start()
+    {
+        info_text.SetActive(false);
+    }
+
+
+    public void OnMouseEnter()
+    {
+        info_text.SetActive(true);
+
+    }
+
+    public void OnMouseExit()
+    {
+        info_text.SetActive(false);
+
+    }
+}
