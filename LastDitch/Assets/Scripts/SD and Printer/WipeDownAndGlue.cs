fcbf69bcd5196f9de465e3c8dea291561c6c18b3
Oisin <C17343216@mydit.ie>
2019-11-19 13:41:12 +0000

    Update

---
+++ LastDitch/Assets/Scripts/SD and Printer/WipeDownAndGlue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WipeDownAndGlue : MonoBehaviour
+{
+    ProgressTracker progressScript;
+    
+    void Start()
+    {
+        progressScript = GetComponent<ProgressTracker>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+	
+}
