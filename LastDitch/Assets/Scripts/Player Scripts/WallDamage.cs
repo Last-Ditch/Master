9d7e9404b839847f1ce6be11540194949d31076d
Oisin <C17343216@mydit.ie>
2019-12-09 16:16:45 +0000

    New Art in

--- LastDitch/Assets/Scripts/Player Scripts/WallDamage.cs
+++ LastDitch/Assets/Scripts/Player Scripts/WallDamage.cs
@@ -19,7 +19,7 @@ public class WallDamage : MonoBehaviour
         if (collision.gameObject.tag == "Interactable")
         {
             //puts in a quad with a hole effect for the target
-            GameObject g = Instantiate(damageeffect, new Vector3(collision.transform.position.x, collision.transform.position.y, -15.55f), Quaternion.LookRotation(Vector3.back, Vector3.up));
+            GameObject g = Instantiate(damageeffect, new Vector3(collision.transform.position.x, collision.transform.position.y, -15.38f), Quaternion.LookRotation(Vector3.back, Vector3.up));
             speaker.PlayOneShot(boom);
         }
     }
